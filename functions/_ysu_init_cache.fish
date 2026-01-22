228ae47313fd34b5839062adb0c5b7c02e682913
Tony Kirkland <tony@tonykirkland.net>
2026-01-22 06:37:03 -0500

    Add function alias caching to _ysu_init_cache.fish
    Added function alias caching alongside existing abbreviation caching:
    - Added _ysu_alias_names and _ysu_alias_values parallel arrays
    - Iterate through all Fish functions using functions -n
    - Parse function definitions to extract the wrapped command
    - Handle simple alias functions (single command with $argv)
    - Handle complex functions (store first line as hint)
    - Follows Fish parallel array pattern for O(1) lookup

--- functions/_ysu_init_cache.fish
+++ functions/_ysu_init_cache.fish
@@ -1,11 +1,17 @@
 # Cache initialization function for you-should-use Fish plugin
 # Ported from fish-abbreviation-tips caching pattern
-# This caches abbreviations to parallel arrays for O(1) lookup performance
+# This caches abbreviations and function aliases to parallel arrays for O(1) lookup performance
 
-function _ysu_init_cache --description "Initialize abbreviation cache for YSU plugin"
+function _ysu_init_cache --description "Initialize abbreviation and function alias cache for YSU plugin"
     # Clear existing abbreviation cache
     set -g _ysu_abbr_keys
     set -g _ysu_abbr_values
+
+    # Clear existing function alias cache
+    # _ysu_alias_names: list of function names
+    # _ysu_alias_values: list of function bodies (first line/command)
+    set -g _ysu_alias_names
+    set -g _ysu_alias_values
 
     # Parse abbr --show output
     # Format: abbr -a -- key 'value'
@@ -36,6 +42,63 @@ function _ysu_init_cache --description "Initialize abbreviation cache for YSU pl
             set -l val $parts[3]
             set val (string replace -r "^['\"](.*)['\"]\$" '$1' -- $val)
             set -a _ysu_abbr_values $val
+        end
+    end
+
+    # Cache function aliases (Fish functions act as aliases)
+    # Get all function names and extract their bodies
+    for func_name in (functions -n 2>/dev/null)
+        # Skip internal/private functions (starting with _) unless they're user-defined short aliases
+        # Also skip fish builtins and completion functions
+        # We cache all functions to allow alias detection
+        set -a _ysu_alias_names $func_name
+
+        # Get function definition to extract the body/command it wraps
+        # functions <name> returns the full definition, we extract the command
+        set -l func_def (functions $func_name 2>/dev/null)
+        if test -n "$func_def"
+            # Parse function body: look for the actual command being wrapped
+            # Function format:
+            # function name [--description 'desc']
+            #     command $argv
+            # end
+            # We want to extract the command being wrapped (if it's a simple wrapper)
+            set -l body_lines
+            set -l in_body 0
+            for line in $func_def
+                # Skip the function declaration line
+                if string match -qr '^\s*function\s+' $line
+                    set in_body 1
+                    continue
+                end
+                # Skip the end line
+                if string match -qr '^\s*end\s*$' $line
+                    continue
+                end
+                # Collect body lines
+                if test $in_body -eq 1
+                    set -l trimmed (string trim $line)
+                    if test -n "$trimmed"
+                        set -a body_lines $trimmed
+                    end
+                end
+            end
+
+            # If function has a simple body (one command), extract it
+            if test (count $body_lines) -eq 1
+                # Remove $argv from the end if present (common pattern for alias functions)
+                set -l cmd (string replace -r '\s*\$argv\s*$' '' -- $body_lines[1])
+                set -a _ysu_alias_values $cmd
+            else if test (count $body_lines) -gt 0
+                # For complex functions, store the first line as hint
+                set -a _ysu_alias_values $body_lines[1]
+            else
+                # Empty function or parse error, store function name as value
+                set -a _ysu_alias_values $func_name
+            end
+        else
+            # Could not get definition, store function name
+            set -a _ysu_alias_values $func_name
         end
     end
 
